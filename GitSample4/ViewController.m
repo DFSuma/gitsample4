e675a96150e944494b53c0523b742c5733932831
“DFSuma” <sumaiosdeveloper@gmail.com>
2016-10-07 10:25:34 +0800

    Initial Commit

---
+++ GitSample4/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitSample4
+//
+//  Created by YF Kong on 07/10/2016.
+//  Copyright © 2016 YF Kong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
