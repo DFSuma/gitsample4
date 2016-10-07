dc0f4452c479ef2a7b5ba9608dde2d57c87f3ca2
“DFSuma” <sumaiosdeveloper@gmail.com>
2016-10-07 10:33:34 +0800

    removed comment and added viewdidappear

--- GitSample4/ViewController.m
+++ GitSample4/ViewController.m
@@ -19,10 +19,14 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated{
+    // View did appeear
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
 
 
